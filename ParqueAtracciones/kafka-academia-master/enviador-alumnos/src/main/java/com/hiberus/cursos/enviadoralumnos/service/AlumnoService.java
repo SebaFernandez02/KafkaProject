fb0608e5a3a0a1bdffd7922c5c5489b4eb84cb39
Sebastián Fernández <111531796+SebaFernandez02@users.noreply.github.com>
2025-01-20 09:36:30 -0300

    Delete ParqueAtracciones directory

--- ParqueAtracciones/kafka-academia-master/enviador-alumnos/src/main/java/com/hiberus/cursos/enviadoralumnos/service/AlumnoService.java
+++
@@ -1,9 +0,0 @@
-package com.hiberus.cursos.enviadoralumnos.service;
-
-
-import com.hiberus.cursos.enviadoralumnos.dto.AlumnoDTO;
-
-public interface AlumnoService {
-
-    void crear(AlumnoDTO alumno);
-}
--- ParqueAtracciones/kafka-academia-master/usuariosCRUD/src/main/java/com/usuarios/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.usuarios.service;
-
-import com.usuarios.dto.UserDto;
-
-public interface UserService {
-
-    void createUser(UserDto user);
-}
